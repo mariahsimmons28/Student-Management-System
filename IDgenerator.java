c4a6abedc5512991fdd407d619fcefd3c23a222f
mariahsimmons28 <36414014+mariahsimmons28@users.noreply.github.com>
2020-04-16 21:32:19 -0400

    Update IDgenerator.java
    Updated IDgenerator that creates a username, email, and password for a new student

--- IDgenerator.java
+++ IDgenerator.java
@@ -3,13 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package studentmanager;
+
 import java.util.*;
+import java.util.stream.IntStream;
 public class IDgenerator {
     public static void main(String[]args){
         int length = 10;
+        String fn = "Nia";
+        String ln = "Simmons";
+        System.out.println((Password(length,fn,ln)));
+        User(fn,ln);
         
-        //System.out.println((Password(length,)));
+        
     }
     public static char [] Password(int len,String first, String last){
        System.out.println("Please Wait. Generating password......");
@@ -31,20 +36,50 @@ public class IDgenerator {
            password[i]= all.charAt(ran.nextInt(all.length()));
        }
        return password;
+       
+       //System.out.print(password);
         
     }
+    
     public static void User(String first, String last){
+    	System.out.println("\n");
        System.out.println("Please Wait. Generating student username......");
        System.out.println("---------------------------------------");
        System.out.println("The student's username is: ");
        
        String fchar = first;//string with first name 
        String lchar = last;//string with last name 
-       String finit = fchar.substring(0);
-       String linit = lchar.substring(0,3);
+       String finit = fchar.substring(0,1);// first letter of first name
+       String linit = lchar.substring(0,3);// first 3 letters of last name 
        String numbers = "0123456789";
        
-       
+       //random number generation for the username
+       Random ran = new Random();
+       //range values 
+       int min = 1000;
+       int max = 9999;
+       int idnum = ran.nextInt((max-min)+1);// holds range of number from 1000 to 9999 and prints a number in the range 
+       //username creation 
+       String user = finit +linit+idnum; // holds the user name 
+       System.out.println(user);
+       System.out.println("\n");
+       Email(user);
+   	  
+   	   //System.out.print(email);//prints the users email to the console
+   	   
         
+    }
+    public static void getUser(){
+    	
+    }
+    public static void Email(String inuser) {
+    	String domain = "@students.vsu.edu";
+    	String user = inuser;
+    	String email = inuser + domain; 
+    	
+    	System.out.println("Please Wait. Generating student email address......");
+        System.out.println("---------------------------------------");
+        System.out.println("\n");
+    	System.out.print(email);//prints the users email to the console 
     }
 }
